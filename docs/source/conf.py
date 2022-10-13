bc39403883eee7533c14f7bba3786302604dac89
conradpringiers <74701483+conradpringiers@users.noreply.github.com>
2022-10-13 13:48:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Gam&+'
+copyright = '2021, Gamandplus'
+author = 'COnix'
 
 release = '0.1'
 version = '0.1.0'
